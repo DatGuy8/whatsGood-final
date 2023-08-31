a14f57beeb2692260a819de56bb85e72b74ae7c6
John Tran <namesjohntran@gmail.com>
2023-08-31 02:08:59 -0700

    commit

--- src/main/java/com/johntran/whatsgoodfinal/WhatsGoodFinalApplication.java
+++ src/main/java/com/johntran/whatsgoodfinal/WhatsGoodFinalApplication.java
@@ -1,11 +1,7 @@
 package com.johntran.whatsgoodfinal;
 
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WhatsGoodFinalApplication {
@@ -13,16 +9,19 @@ public class WhatsGoodFinalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WhatsGoodFinalApplication.class, args);
 	}
-	@Bean
-    public TomcatServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        Connector ajpConnector = new Connector("AJP/1.3");
-        ajpConnector.setPort(9090);
-        ajpConnector.setSecure(false);
-        ajpConnector.setAllowTrace(false);
-        ajpConnector.setScheme("http");
-	((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
-        tomcat.addAdditionalTomcatConnectors(ajpConnector);
-        return tomcat;
-    }
+	
+	// for AWS DEPLOYMENT
+	
+//	@Bean
+//    public TomcatServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        Connector ajpConnector = new Connector("AJP/1.3");
+//        ajpConnector.setPort(9090);
+//        ajpConnector.setSecure(false);
+//        ajpConnector.setAllowTrace(false);
+//        ajpConnector.setScheme("http");
+//	((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+//        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+//        return tomcat;
+//    }
 }
